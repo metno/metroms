597336a53f8c5f966fdf9352662a9c541c43ad94
Geir Arne Waagbø <geiraw@pc4127.pc.met.no>
2014-02-12 13:33:57 +0100

    Can now build and run cice

---
+++ apps/build_cice.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -x
+
+workingdir=${PWD} 
+cd ../
+metroms_base=${PWD} 
+cd ../
+tup=${PWD}
+
+# Build ESMF
+cd ${tup}/tmproms
+mkdir cice
+export CICE_DIR=${tup}/tmproms/cice/
+cd $CICE_DIR
+
+# Unpack standard source files
+tar -xf ${metroms_base}/static_libs/cice_4_1.tar.gz
+
+# Copy modified source files
+cp -auv $workingdir/common/modified_src/cice ${tup}/tmproms
+
+./comp_ice
+
+set +x
