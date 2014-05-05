f70327bc89f542e5d599d315240af92a4507f161
Geir Arne Waagbø <geiraw@pc4127.pc.met.no>
2014-05-05 15:53:41 +0200

    Debug ROMS run

---
+++ apps/arctic-20km/modified_src/nl_ocean.h
@@ -0,0 +1,320 @@
+      MODULE ocean_control_mod
+!
+!svn $Id: nl_ocean.h 553 2011-04-22 21:30:04Z arango $
+!================================================== Hernan G. Arango ===
+!  Copyright (c) 2002-2011 The ROMS/TOMS Group                         !
+!    Licensed under a MIT/X style license                              !
+!    See License_ROMS.txt                                              !
+!=======================================================================
+!                                                                      !
+!  ROMS/TOMS Nonlinear Model Driver:                                   !
+!                                                                      !
+!  This driver executes ROMS/TOMS standard nonlinear model.  It        !
+!  controls the initialization, time-stepping, and finalization        !
+!  of the nonlinear model execution following ESMF conventions:        !
+!                                                                      !
+!     ROMS_initialize                                                  !
+!     ROMS_run                                                         !
+!     ROMS_finalize                                                    !
+!                                                                      !
+!=======================================================================
+!
+      implicit none
+
+      PRIVATE
+      PUBLIC  :: ROMS_initialize
+      PUBLIC  :: ROMS_run
+      PUBLIC  :: ROMS_finalize
+
+      CONTAINS
+
+      SUBROUTINE ROMS_initialize (first, mpiCOMM)
+!
+!=======================================================================
+!                                                                      !
+!  This routine allocates and initializes ROMS/TOMS state variables    !
+!  and internal and external parameters.                               !
+!                                                                      !
+!=======================================================================
+!
+      USE mod_param
+      USE mod_parallel
+#ifdef VERIFICATION
+      USE mod_fourdvar
+#endif
+      USE mod_iounits
+      USE mod_scalars
+
+#ifdef MCT_LIB
+!
+# ifdef AIR_OCEAN
+      USE ocean_coupler_mod, ONLY : initialize_ocn2atm_coupling
+# endif
+# ifdef WAVES_OCEAN
+      USE ocean_coupler_mod, ONLY : initialize_ocn2wav_coupling
+# endif
+#endif
+!
+!  Imported variable declarations.
+!
+      logical, intent(inout) :: first
+
+      integer, intent(in), optional :: mpiCOMM
+!
+!  Local variable declarations.
+!
+      logical :: allocate_vars = .TRUE.
+
+#ifdef DISTRIBUTE
+      integer :: MyError, MySize
+#endif
+      integer :: ng, thread
+
+#ifdef DISTRIBUTE
+!
+!-----------------------------------------------------------------------
+!  Set distribute-memory (MPI) world communictor.
+!-----------------------------------------------------------------------
+!
+      IF (PRESENT(mpiCOMM)) THEN
+        OCN_COMM_WORLD=mpiCOMM
+      ELSE
+        OCN_COMM_WORLD=MPI_COMM_WORLD
+      END IF
+      CALL mpi_comm_rank (OCN_COMM_WORLD, MyRank, MyError)
+      CALL mpi_comm_size (OCN_COMM_WORLD, MySize, MyError)
+#endif
+!
+!-----------------------------------------------------------------------
+!  On first pass, initialize model parameters a variables for all
+!  nested/composed grids.  Notice that the logical switch "first"
+!  is used to allow multiple calls to this routine during ensemble
+!  configurations.
+!-----------------------------------------------------------------------
+!
+      IF (first) THEN
+        first=.FALSE.
+!
+!  Initialize parallel control switches. These scalars switches are
+!  independent from standard input parameters.
+!
+        CALL initialize_parallel
+!
+!  Read in model tunable parameters from standard input. Allocate and
+!  initialize variables in several modules after the number of nested
+!  grids and dimension parameters are known.
+!
+		WRITE (stdout,10)
+ 10     FORMAT (/,' Calling inp_par',/)
+        CALL inp_par (iNLM)
+        IF (exit_flag.ne.NoError) RETURN
+!
+!  Initialize internal wall clocks. Notice that the timings does not
+!  includes processing standard input because several parameters are
+!  needed to allocate clock variables.
+!
+        WRITE (stdout,11)
+ 11     FORMAT (/,' Init clocks',/)
+!
+        DO ng=1,Ngrids
+!$OMP PARALLEL DO PRIVATE(thread) SHARED(numthreads)
+          DO thread=0,numthreads-1
+            CALL wclock_on (ng, iNLM, 0)
+          END DO
+!$OMP END PARALLEL DO
+        END DO
+!
+!  Allocate and initialize all model state arrays.
+!
+		WRITE (stdout,12)
+ 12     FORMAT (/,' Allocate arrays',/)
+        CALL mod_arrays (allocate_vars)
+
+#ifdef VERIFICATION
+!
+!  Allocate and initialize observation arrays.
+!
+        CALL initialize_fourdvar
+#endif
+      END IF
+
+#if defined MCT_LIB && (defined AIR_OCEAN || defined WAVES_OCEAN)
+!
+!-----------------------------------------------------------------------
+!  Initialize coupling streams between model(s).
+!-----------------------------------------------------------------------
+!
+      WRITE (stdout,13)
+ 13   FORMAT (/,' Initialize coupling streams',/)
+      DO ng=1,Ngrids
+# ifdef AIR_OCEAN
+        CALL initialize_ocn2atm_coupling (ng, MyRank)
+# endif
+# ifdef WAVES_OCEAN
+        CALL initialize_ocn2wav_coupling (ng, MyRank)
+# endif
+      END DO
+#endif
+!
+!-----------------------------------------------------------------------
+!  Initialize nonlinear model state variables over all nested grids,
+!  if applicable.
+!-----------------------------------------------------------------------
+!
+      DO ng=1,Ngrids
+        CALL initial (ng)
+        IF (exit_flag.ne.NoError) RETURN
+      END DO
+!
+!  Initialize run or ensemble counter.
+!
+      Nrun=1
+
+#ifdef VERIFICATION
+!
+!  Create out NetCDF file containing model solution at observation
+!  locations.
+!
+      IF (Nrun.eq.1) THEN
+        DO ng=1,Ngrids
+          LdefMOD(ng)=.TRUE.
+          wrtNLmod(ng)=.TRUE.
+          CALL def_mod (ng)
+          IF (exit_flag.ne.NoError) RETURN
+        END DO
+      END IF
+#endif
+
+      RETURN
+      END SUBROUTINE ROMS_initialize
+
+      SUBROUTINE ROMS_run (RunInterval)
+!
+!=======================================================================
+!                                                                      !
+!  This routine runs ROMS/TOMS nonlinear model for the specified time  !
+!  interval (seconds), RunInterval.                                    !
+!                                                                      !
+!=======================================================================
+!
+      USE mod_param
+      USE mod_parallel
+#ifdef VERIFICATION
+      USE mod_fourdvar
+#endif
+      USE mod_iounits
+      USE mod_scalars
+!
+!  Imported variable declarations.
+!
+      real(r8), intent(in) :: RunInterval            ! seconds
+!
+!  Local variable declarations.
+!
+      integer :: ng
+!
+!-----------------------------------------------------------------------
+!  Time-step nonlinear model over all nested grids, if applicable.
+!-----------------------------------------------------------------------
+!
+      DO ng=1,Ngrids
+        IF (Master) THEN
+          WRITE (stdout,10) 'NL', ng, ntstart(ng), ntend(ng)
+        END IF
+      END DO
+
+#ifdef SOLVE3D
+      CALL main3d (RunInterval)
+#else
+      CALL main2d (RunInterval)
+#endif
+      IF (exit_flag.ne.NoError) RETURN
+!
+ 10   FORMAT (/,1x,a,1x,'ROMS/TOMS: started time-stepping:',            &
+     &        ' (Grid: ',i2.2,' TimeSteps: ',i8.8,' - ',i8.8,')',/)
+
+      RETURN
+      END SUBROUTINE ROMS_run
+
+      SUBROUTINE ROMS_finalize
+!
+!=======================================================================
+!                                                                      !
+!  This routine terminates ROMS/TOMS nonlinear model execution.        !
+!                                                                      !
+!=======================================================================
+!
+      USE mod_param
+      USE mod_parallel
+      USE mod_iounits
+      USE mod_ncparam
+      USE mod_scalars
+!
+!  Local variable declarations.
+!
+      integer :: Fcount, ng, thread
+
+#ifdef VERIFICATION
+!
+!-----------------------------------------------------------------------
+!  Compute and report model-observation comparison statistics.
+!-----------------------------------------------------------------------
+!
+      IF (exit_flag.eq.NoError) THEN
+        DO ng=1,Ngrids
+          CALL stats_modobs (ng)
+        END DO
+      END IF
+#endif
+!
+!-----------------------------------------------------------------------
+!  If blowing-up, save latest model state into RESTART NetCDF file.
+!-----------------------------------------------------------------------
+!
+!  If cycling restart records, write solution into the next record.
+!
+      IF (exit_flag.eq.1) THEN
+        DO ng=1,Ngrids
+          IF (LwrtRST(ng)) THEN
+            IF (Master) WRITE (stdout,10)
+ 10         FORMAT (/,' Blowing-up: Saving latest model state into ',   &
+     &                ' RESTART file',/)
+            Fcount=RST(ng)%Fcount
+            IF (LcycleRST(ng).and.(RST(ng)%Nrec(Fcount).ge.2)) THEN
+              RST(ng)%Rindex=2
+              LcycleRST(ng)=.FALSE.
+            END IF
+            blowup=exit_flag
+            exit_flag=NoError
+            CALL wrt_rst (ng)
+          END IF
+        END DO
+      END IF
+!
+!-----------------------------------------------------------------------
+!  Stop model and time profiling clocks.  Close output NetCDF files.
+!-----------------------------------------------------------------------
+!
+!  Stop time clocks.
+!
+      IF (Master) THEN
+        WRITE (stdout,20)
+ 20     FORMAT (/,' Elapsed CPU time (seconds):',/)
+      END IF
+!
+      DO ng=1,Ngrids
+!$OMP PARALLEL DO PRIVATE(thread) SHARED(numthreads)
+        DO thread=0,numthreads-1
+          CALL wclock_off (ng, iNLM, 0)
+        END DO
+!$OMP END PARALLEL DO
+      END DO
+!
+!  Close IO files.
+!
+      CALL close_out
+
+      RETURN
+      END SUBROUTINE ROMS_finalize
+
+      END MODULE ocean_control_mod
