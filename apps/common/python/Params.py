230c0661ff337420015495d22861ef27fc9caded
Geir Arne Waagbø <geiraw@pc4127.pc.met.no>
2014-05-06 16:37:14 +0200

    Bugfix

--- apps/common/python/Params.py
+++ apps/common/python/Params.py
@@ -61,7 +61,7 @@ class Params(object):
         ['RIVERFILE',GlobalParams.COMMONPATH+"/rivers/A20_rivers_openBering35.nc"],
         ['FORCEFILES',"4"],
         ['ROMS/External/coupling.dat', GlobalParams.COMMONPATH + "/../../../tmproms/roms_src/ROMS/External/coupling.dat"]
-        ['ocean.in', self.ROMSINFILE ]
+        ['ocean.in', self.ROMSINFILE ],
         ['cice.in', GlobalParams.COMMONPATH + "/../../../tmproms/cice/rundir/ice_in" ]
         ]
         ########################################################################
