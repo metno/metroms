da55737e34da8cea82c3dc6f3d5f4b424bde430a
Geir Arne Waagbø <geiraw@pc4127.pc.met.no>
2014-05-16 10:43:51 +0200

    Improve cice build

--- apps/build_cice.sh
+++ apps/build_cice.sh
@@ -15,6 +15,9 @@ tar -xf ${metroms_base}/static_libs/cice5.tar.gz
 export CICE_DIR=${tup}/tmproms/cice
 cd $CICE_DIR
 
+export MCT_INCDIR=${tup}/tmproms/roms_src/Lib/MCT/include
+export MCT_LIBDIR=${tup}/tmproms/roms_src/Lib/MCT/lib
+
 
 # Copy modified source files
 cp -auv $workingdir/common/modified_src/cice ${tup}/tmproms
@@ -24,5 +27,7 @@ cp -auv $workingdir/common/modified_src/cice ${tup}/tmproms
 # Build a library (for use in the ROMS build)
 cd $CICE_DIR/rundir/compile
 ar rcv libcice.a *.o
+
+cp $workingdir/common/modified_src/cice/input_templates/ice_in ${tup}/tmproms/cice/rundir/
 
 set +x
