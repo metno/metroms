70befd0fcc4bca76900eefcc11705b864cd61ce9
Geir Arne Waagbø <geiraw@pc4127.pc.met.no>
2014-05-23 09:50:57 +0200

    Debug

--- apps/arctic-20km/modified_src/Linux-gfortran.mk
+++ apps/arctic-20km/modified_src/Linux-gfortran.mk
@@ -78,7 +78,7 @@ endif
 ifdef USE_DEBUG
            FFLAGS += -g -fbounds-check
 else
-           FFLAGS += -fbacktrace -g -fbounds-check
+           FFLAGS += -fbacktrace -g
 #           FFLAGS += -O3 -ffast-math
 endif
 
