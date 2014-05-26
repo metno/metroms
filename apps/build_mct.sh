e39174698f0290923c60985109ffbcdb145272d5
Geir Arne Waagbø <geiraw@pc4127.pc.met.no>
2014-05-26 11:23:51 +0200

    Build MCT as a separate module to improve dependency graph

---
+++ apps/build_mct.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -x
+
+workingdir=${PWD} 
+cd ../
+metroms_base=${PWD} 
+cd ../
+tup=${PWD}
+
+# Build MCT
+mkdir -p ${tup}/tmproms
+cd ${tup}/tmproms
+# Unpack standard source files
+tar -xf ${metroms_base}/static_libs/mct-2.8.tar.gz
+export MCT_DIR=${tup}/tmproms/MCT
+cd $MCT_DIR
+
+./configure FC=gfortran --prefix=$MCT_DIR
+make install
+
+set +x
