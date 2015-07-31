bdc6477532f3ddb215836ab3445301984234fd30
Nils Melsom Kristensen <nilsmk@met.no>
2015-07-31 15:46:56 +0200

    Changed some default CPP options. (Advections schemes mostly). Put TS_MPDATA as defined, to deal with issue of negative salinities

--- apps/arctic-20km/include/arctic-20km.h
+++ apps/arctic-20km/include/arctic-20km.h
@@ -16,40 +16,44 @@
 #undef  PERFECT_RESTART    /* use to include perfect restart variables */
 #define CURVGRID           /* define if using  curvilinear coordinate grid*/
 
+#undef  DIAGNOSTICS_UV     /* define if writing out dynamics to diagnostics file*/
+#undef  DIAGNOSTICS_TS     /* define if writing out tracers  to diagnostics file*/
+
 #define UV_ADV             /* turn ON or OFF advection terms */
 #define UV_COR             /* turn ON or OFF Coriolis term */
-#undef  UV_VIS2            /* turn ON or OFF Laplacian horizontal mixing, Frode off */
+#undef  UV_VIS2            /* turn ON or OFF Laplacian horizontal mixing */
 #undef  UV_VIS4            /* turn ON or OFF biharmonic horizontal mixing */
-#define UV_SADVECTION      /* turn ON or OFF splines vertical advection */
+#undef  UV_SADVECTION      /* turn ON or OFF splines vertical advection */
 #define UV_QDRAG           /* turn ON or OFF quadratic bottom friction */
+#undef  UV_SMAGORINSKY
 
 #undef  VISC_GRID          /* viscosity coefficient scaled by grid size */
 #define NONLIN_EOS         /* define if using nonlinear equation of state */
-#undef  MIX_GEO_UV         /* mixing on geopotential (constant Z) surfaces */
 #undef  WJ_GRADP           /* Weighted density Jacobian (Song, 1998) */
 #define DJ_GRADPS          /* Splines density  Jacobian (Shchepetkin, 2000) */
 #undef  DIFF_GRID          /* diffusion coefficient scaled by grid size */
 
 #undef  TS_DIF2            /* turn ON or OFF Laplacian horizontal mixing */
 #undef  TS_DIF4            /* turn ON or OFF biharmonic horizontal mixing */
-#define TS_U3HADVECTION    /* define if 3rd-order upstream horiz. advection */
+#undef  TS_U3HADVECTION    /* define if 3rd-order upstream horiz. advection */
 #undef  TS_A4HADVECTION    /* define if 4th-order Akima horiz. advection */
 #undef  TS_C4HADVECTION    /* define if 4th-order centered horizontal advection */
 
-#undef  TS_MPDATA          /* define if recursive MPDATA 3D advection */
+#define TS_MPDATA          /* define if recursive MPDATA 3D advection */
 
 #undef  TS_A4VADVECTION    /* define if 4th-order Akima vertical advection */
-#define TS_SVADVECTION     /* define if splines vertical advection */
+#undef  TS_SVADVECTION     /* define if splines vertical advection */
 #undef  TS_C4VADVECTION    /* define if 4th-order centered vertical advection */
 #undef  TS_SMAGORINSKY     /* define if Smagorinsky-like diffusion */
 
-#define MIX_GEO_TS         /* mixing on geopotential (constant Z) surfaces, Frode undef */
-#undef  MIX_S_UV           /* mixing along constant S-surfaces,   Frode -*/
+#undef  MIX_GEO_TS         /* mixing on geopotential (constant Z) surfaces */
+#undef  MIX_S_UV           /* mixing along constant S-surfaces */
+#undef  MIX_GEO_UV         /* mixing on geopotential (constant Z) surfaces */
 
 #define SALINITY           /* define if using salinity */
 #define SOLVE3D            /* define if solving 3D primitive equations */
 #undef  BODYFORCE          /* define if applying stresses as bodyforces */
-#define SPLINES            /* turn ON or OFF parabolic splines reconstruction */
+#undef  SPLINES            /* turn ON or OFF parabolic splines reconstruction */
 #define MASKING            /* define if there is land in the domain */
 #define AVERAGES           /* define if writing out time-averaged data */
 #undef  AVERAGES_DETIDE    /*use if writing out NLM time-averaged detided fields*/
@@ -61,9 +65,9 @@
 #undef  STATIONS_CGRID     /* define if extracting data at native C-grid */
 
 #undef  BVF_MIXING         /* define if Brunt_Vaisala frequency mixing */
-#define LMD_MIXING         /* define if Large et al. (1994) interior closure */
+#undef  LMD_MIXING         /* define if Large et al. (1994) interior closure */
 #undef  MY25_MIXING        /* define if Mellor/Yamada level-2.5 mixing */
-#undef  GLS_MIXING         /* Activate Generic Length-Scale mixing */
+#define GLS_MIXING         /* Activate Generic Length-Scale mixing */
 
 #ifdef GLS_MIXING
 # define N2S2_HORAVG       /* Activate horizontal smoothing of buoyancy/shear */
@@ -114,7 +118,7 @@
 # undef  CLOUDS      
 # undef  LONGWAVE_OUT      /* compute outgoing longwave radiation */
 # define LONGWAVE          /* Compute net longwave radiation internally */
-# define COOL_SKIN         /* turn ON or OFF cool skin correction *//* Ikke def hos Frode*/
+# define COOL_SKIN         /* turn ON or OFF cool skin correction */
 # undef  SHORTWAVE
 #endif
 
