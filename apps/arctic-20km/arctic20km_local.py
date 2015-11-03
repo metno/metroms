d9fb161b65f08e20aede2e4bd16633ed9e60db52
Nils Melsom Kristensen <nilsmk@met.no>
2015-11-03 16:15:24 +0100

    minor update to name
    Former-commit-id: 8b7745195457efa3198e81f5c27498ce87d96a43

--- apps/arctic-20km/arctic20km_local.py
+++ apps/arctic-20km/arctic20km_local.py
@@ -15,14 +15,14 @@ ycpu=2
 # Set cpus for CICE:
 icecpu=2
 # Choose a predefined ROMS-application:
-app='a20' # Arctic-20km
+app='arctic-20km' # Arctic-20km
 
 start_date = datetime(1997,01,16,12)
 end_date   = datetime(1997,02,01,12)
 
 
 #NMK: If restart, set irestart=
-a20params=Params(app,xcpu,ycpu,start_date,end_date,nrrec=0,cicecpu=icecpu,restart=True)
+a20params=Params(app,xcpu,ycpu,start_date,end_date,nrrec=0,cicecpu=icecpu,restart=False)
 #a20params=Params(app,xcpu,ycpu,fclen=(744*12),irestart=0,cicecpu=icecpu)
 #a20params.RUNPATH="/disk1/tmp"
 #a20params.ROMSINFILE=a20params.RUNPATH+"/roms___.in"
