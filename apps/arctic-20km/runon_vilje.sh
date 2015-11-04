19502a9f67cb468a8327da2434f83534c6355dd5
Nils Melsom Kristensen <nilsmk@met.no>
2015-11-04 09:46:18 +0100

    minor tweak with logfiles in runon_vilje-script
    Former-commit-id: c988bdb38737bd4cbf82d9169f4f513b16e1e53c

--- apps/arctic-20km/runon_vilje.sh
+++ apps/arctic-20km/runon_vilje.sh
@@ -2,10 +2,13 @@
 #PBS -A mifa01hi
 #PBS -l walltime=12:00:00
 #PBS -l select=1:ncpus=32:mpiprocs=16:ompthreads=16:mem=29gb
-#PBS -o /work/$USER/tmproms/run/arctic-20km/prep_o.log
 #PBS -j oe
 #PBS -V
-
+datstamp=`date +%Y_%m_%d_%H_%M`
+exec 1>/work/$USER/tmproms/run/arctic-20km/run.log_${datstamp} 2>&1
+# Load modules needed
+source /etc/profile.d/modules.sh
 module load python
+#
 cd ~/metroms/apps/arctic-20km
 python arctic20km_local.py
