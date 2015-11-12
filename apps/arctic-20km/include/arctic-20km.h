9bdccf689500660ddbedb98ffe2a330f058a7f19
Jens Boldingh Debernard <jensd@met.no>
2015-11-12 17:10:05 +0100

    Changed advection schemes in ROMS, and increased diffusion to reduce problems with false water masses near strong gradients in hydrography. This reduces artificial frazil ice production.
    Former-commit-id: 7f1680e50e4e7d0980cca5980e00fe408d06a851

--- apps/arctic-20km/include/arctic-20km.h
+++ apps/arctic-20km/include/arctic-20km.h
@@ -23,11 +23,14 @@
 
 #define UV_ADV             /* turn ON or OFF advection terms */
 #define UV_COR             /* turn ON or OFF Coriolis term */
-#undef  UV_VIS2            /* turn ON or OFF Laplacian horizontal mixing */
-#undef  UV_VIS4            /* turn ON or OFF biharmonic horizontal mixing */
-#define UV_SADVECTION      /* turn ON or OFF splines vertical advection */
+#undef UV_VIS2            /* turn ON or OFF Laplacian horizontal mixing */
+#define  UV_VIS4            /* turn ON or OFF biharmonic horizontal mixing */
+#undef UV_U3ADV_SPLIT     /* use 3rd-order upstream split momentum advection */
+#undef  UV_U3HADVECTION    /* define if 3rd-order upstream horiz. advection */
+#undef  UV_SADVECTION      /* turn ON or OFF splines vertical advection */
+#undef  UV_C4HADVECTION    /* define if 4th-order centered horizontal advection */
 #define UV_QDRAG           /* turn ON or OFF quadratic bottom friction */
-#undef  UV_SMAGORINSKY
+#define  UV_SMAGORINSKY
 
 #undef  VISC_GRID          /* viscosity coefficient scaled by grid size */
 #define NONLIN_EOS         /* define if using nonlinear equation of state */
@@ -35,21 +38,23 @@
 #define DJ_GRADPS          /* Splines density  Jacobian (Shchepetkin, 2000) */
 #undef  DIFF_GRID          /* diffusion coefficient scaled by grid size */
 
-#undef  TS_DIF2            /* turn ON or OFF Laplacian horizontal mixing */
+#define  TS_DIF2            /* turn ON or OFF Laplacian horizontal mixing */
 #undef  TS_DIF4            /* turn ON or OFF biharmonic horizontal mixing */
-#define TS_U3HADVECTION    /* define if 3rd-order upstream horiz. advection */
-#undef  TS_A4HADVECTION    /* define if 4th-order Akima horiz. advection */
+#undef  TS_U3ADV_SPLIT     /* use 3rd-order upstream split tracer advection */
+#undef  TS_U3HADVECTION    /* define if 3rd-order upstream horiz. advection */
+#define  TS_A4HADVECTION    /* define if 4th-order Akima horiz. advection */
 #undef  TS_C4HADVECTION    /* define if 4th-order centered horizontal advection */
 
 #undef  TS_MPDATA          /* define if recursive MPDATA 3D advection */
 
-#undef  TS_A4VADVECTION    /* define if 4th-order Akima vertical advection */
-#define TS_SVADVECTION     /* define if splines vertical advection */
+#define  TS_A4VADVECTION    /* define if 4th-order Akima vertical advection */
 #undef  TS_C4VADVECTION    /* define if 4th-order centered vertical advection */
-#undef  TS_SMAGORINSKY     /* define if Smagorinsky-like diffusion */
+#undef TS_SVADVECTION     /* define if splines vertical advection */
+#undef TS_SMAGORINSKY     /* define if Smagorinsky-like diffusion */
 
-#undef  MIX_GEO_TS         /* mixing on geopotential (constant Z) surfaces */
-#undef  MIX_S_UV           /* mixing along constant S-surfaces */
+#undef  MIX_S_TS           /* mixing on constant S-surfaces */
+#define MIX_GEO_TS         /* mixing on geopotential (constant Z) surfaces */
+#define  MIX_S_UV           /* mixing along constant S-surfaces */
 #undef  MIX_GEO_UV         /* mixing on geopotential (constant Z) surfaces */
 
 #define SALINITY           /* define if using salinity */
@@ -95,7 +100,7 @@
 #undef  ANA_SMFLUX         /* analytical surface momentum stress */
 
 /* CLIMATOLOGY */
-#define M2CLIMATOLOGY      /* define 2D momentum climatology */
+#undef M2CLIMATOLOGY      /* define 2D momentum climatology */
 #define M3CLIMATOLOGY      /* define 3D momentum climatology */
 #define TCLIMATOLOGY       /* define tracers climatology */
 #undef  M2CLM_NUDGING
