0ff5bf022a86a150fe6ed73266e4198949fe9ef9
Nils MK <nilsmk@met.no>
2016-03-02 17:32:31 +0100

    these are the default modules that actually work on vilje

--- apps/modules.sh
+++ apps/modules.sh
@@ -1,10 +1,13 @@
+#!/bin/bash
+source /etc/profile.d/modules.sh
 #module load intelcomp/14.0.1
 #module load mpt/2.06
 #module load netcdf/4.1.3
-module load python/2.7.2
+module load intelcomp mpt netcdf python/2.7.2
 
-module unload intelcomp/12.0.5.220
-module load mpt/2.10
-module load intelcomp/15.0.1
-module load netcdf/4.3.2
-module load hdf5/1.8.14-mpi
+#module unload intelcomp/12.0.5.220
+#module purge
+#module load mpt/2.10 intelcomp/15.0.1 netcdf/4.3.2 hdf5/1.8.14-mpi
+#module load intelcomp/15.0.1
+#module load netcdf/4.3.2
+#module load hdf5/1.8.14-mpi
