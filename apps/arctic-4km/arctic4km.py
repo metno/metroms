d8d0e6f93decfc01a92aa1defa44458fb6a9dcb5
Nils MK <nilsmk@met.no>
2016-03-02 17:30:01 +0100

    new default cpu-distribution
    Former-commit-id: 6db5222a32fd4e181c2f9c431dc2784c7e9eb1b1

--- apps/arctic-4km/arctic4km.py
+++ apps/arctic-4km/arctic4km.py
@@ -12,19 +12,19 @@ from GlobalParams import *
 from Params import *
 from ModelRun import *
 ########################################################################
-import time
-time.sleep(20)
+#import time
+#time.sleep(20)
 ########################################################################
 # Set cpus for ROMS:
-xcpu=6
-ycpu=6
+xcpu=16
+ycpu=12
 # Set cpus for CICE:
-icecpu=12
+icecpu=32
 # Choose a predefined ROMS-application:
 app='arctic-4km' # Arctic-4km
 
 start_date = datetime(1997,01,16,12)
-end_date   = datetime(1997,01,17,12)
+end_date   = datetime(1998,01,16,12)
 
 
 a4params=Params(app,xcpu,ycpu,start_date,end_date,nrrec=-1,cicecpu=icecpu,restart=False)
@@ -35,7 +35,7 @@ print GlobalParams.RUNDIR
 print GlobalParams.COMMONPATH
 
 modelrun.preprocess()
-modelrun.run_roms(Constants.MPI,Constants.DEBUG,Constants.VILJE) #24h hindcast
+modelrun.run_roms(Constants.MPI,Constants.NODEBUG,Constants.VILJE) #24h hindcast
 #modelrun.run_roms(Constants.DRY,Constants.NODEBUG,Constants.MET64) #24h hindcast
 modelrun.postprocess()
 
