917b6d9c05e5481db75b13c754abde41580d9e39
Nils MK <nilsmk@met.no>
2017-06-19 15:08:44 +0200

    new files

---
+++ apps/forecast_myenv.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# if [ $# -le 1 ]
+# then
+#   echo "Usage: $0 [vilje|metlocal]"
+#   echo "Choose architecture..."
+#   exit
+# fi
+
+#export METROMS_MYHOST=metlocal
+export METROMS_MYHOST=vilje
+#export METROMS_MYHOST=$1
+
+#export METROMS_MYARCH=Linux
+
+if [ "$METROMS_MYHOST" == "metlocal" ]; then
+    export METROMS_BASEDIR=/disk1/$USER
+    export METROMS_TMPDIR=/disk1/$USER
+elif [ "$METROMS_MYHOST" == "vilje" ]; then
+    export METROMS_BASEDIR=$HOME/sea/ROMS/metroms
+    export METROMS_TMPDIR=$HOME/run/
+    export METROMS_APPDIR=$HOME/sea/ROMS/metroms_apps
+else
+    echo "Undefined METROMS_MYHOST ", $METROMS_MYHOST
+fi
+
+
+export PYTHONPATH=$PYTHONPATH:$METROMS_BASEDIR/apps/common/python/
