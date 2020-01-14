93c4b3fc721282d4f263adc90487e23b4662b513
Keguang Wang <keguang.wang@met.no>
2020-01-14 10:41:33 +0100

    add more env variables in myenv.sh

--- apps/myenv.bash
+++ apps/myenv.bash
@@ -1,7 +1,19 @@
 #!/bin/bash
+# 08/01/2020 by keguangw@met.no to include more environment variables
 
-export METROMS_MYHOST=$1
-echo "loading $METROMS_MYHOST paths"
+set -x
+
+if [ $# -eq 2 ]; then
+   export METROMS_MYHOST=$1
+   export app=$2
+   echo "loading $METROMS_MYHOST path for $app"
+elif [ $# -eq 1 ]; then
+   export METROMS_MYHOST=$1
+   echo "loading $METROMS_MYHOST paths"
+else
+   echo "Undefined METROMS_MYHOST/APPLICATION !!! "
+   return
+fi
 
 if [ "$METROMS_MYHOST" == "metlocal" ]; then
     export METROMS_BASEDIR=/disk1/$USER
@@ -39,11 +51,22 @@ elif [ "$METROMS_MYHOST" == "met_ppi" ]; then
 	export METROMS_BLDDIR=$METROMS_TMPDIR
 	export METROMS_APPDIR=$HOME/metroms_apps
     fi
-elif [ "$METROMS_MYHOST" == "elvis" ] || [ "$METROMS_MYHOST" == "nebula" ]; then
+elif [ "$METROMS_MYHOST" == "alvin" ] || [ "$METROMS_MYHOST" == "elvis" ]; then
     export METROMS_BASEDIR=$HOME/metroms
     export METROMS_TMPDIR=/nobackup/forsk/$USER/metroms_run
     export METROMS_BLDDIR=$METROMS_TMPDIR
     export METROMS_APPDIR=$HOME/metroms_apps
+elif [ "$METROMS_MYHOST" == "nebula" ] ; then
+    export METROMS_BASEDIR=$HOME/metroms
+    export METROMS_TMPDIR=/nobackup/forsk/$USER/metroms_run
+    export METROMS_BLDDIR=$METROMS_TMPDIR
+    export METROMS_APPDIR=$HOME/metroms_apps
+    export WORKDIR=$METROMS_TMPDIR/..
+    export FORCDIR=$WORKDIR/Forcing/$app
+    export INITDIR=$WORKDIR/Initial/$app
+    export DATADIR=$WORKDIR/Data/$app
+    export RUNDIR=$METROMS_TMPDIR/$app/run
+    mkdir -p $RUNDIR
 elif [ "$METROMS_MYHOST" == "stratus" ]; then
     if [ "$USER" == "metno_op" ]; then
 	export METROMS_BASEDIR=$HOME/sea/ROMS/metroms
@@ -57,5 +80,25 @@ else
     echo "Undefined METROMS_MYHOST ", $METROMS_MYHOST
 fi
 
+export ROMSGRD=$METROMS_APPDIR/grid
+export CICEGRD=$METROMS_APPDIR/cice_input_grid
+export MCT_DIR=$METROMS_TMPDIR/MCT
+export METROMS_PYTHON=$METROMS_BASEDIR/apps/common/python
+export PYTHONPATH=$PYTHONPATH:$METROMS_PYTHON
 
-export PYTHONPATH=$PYTHONPATH:$METROMS_BASEDIR/apps/common/python/
+if [ "$METROMS_MYHOST" == "metlocal" ]; then
+    export FORT=gfortran
+    export SITE=Ubuntu
+elif [ "$METROMS_MYHOST" == "met_ppi" ]; then
+    export FORT=ifort
+    export SITE=MET_PPI
+elif [ "$METROMS_MYHOST" == "vilje" ]; then
+    export FORT=ifort
+    export SITE=NTNU.vilje
+else
+    export FORT=ifort
+    export SITE=$METROMS_MYHOST
+fi
+
+set +x
+
