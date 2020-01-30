43d51d48a134445cb92c9d44caa2662d2e79f71c
Keguang Wang <keguang.wang@met.no>
2020-01-30 14:24:15 +0000

    simplify ice_da

--- apps/common/modified_src/cice5.1.2/source/ice_da.F90
+++ apps/common/modified_src/cice5.1.2/source/ice_da.F90
@@ -9,10 +9,12 @@
       module ice_da
 
       use ice_kinds_mod
-      use ice_constants, only: c0, c1, puny, &
+      use ice_constants, only: c0, c1, p1, p01, secday, puny, &
                                field_loc_center, field_type_scalar
       use ice_blocks, only: nx_block, ny_block
       use ice_domain_size, only: ncat, max_blocks, max_ntrcr
+      use ice_communicate, only: my_task, master_task
+      use ice_fileunits, only: nu_diag
       use ice_grid, only: tmask
       use ice_forcing, only: dbug
       use ice_state, only: aicen, vicen, vsnon, trcrn, ntrcr, bound_state, &
@@ -45,38 +47,13 @@
       real (kind=dbl_kind), dimension (:,:,:), allocatable :: &
          aice_obs     ,  & ! observed SIC 
          aice_obs_err ,  & ! observed SIC std
-         aice_mod_err ,  & ! modeled SIC std
-         aice_gain    ,  & ! Kalman gain for aice     
-         aice_nw           ! Optimal nudging weight for SIC
-
-      real (kind=dbl_kind), dimension (:,:,:), allocatable :: &
          vice_obs     ,  & ! observed ice volume (m) 
          vice_obs_err ,  & ! observed ice volume std (m) 
-         vice_mod_err ,  & ! modeled ice volume std (m) 
-         vice_gain    ,  & ! Kalman gain for vice
-         vice_nw           ! optimal nudging weigth for SIV
-
-      real (kind=dbl_kind), dimension (:,:,:), allocatable :: &
          vsno_obs     ,  & ! observed snow volume (m) 
-         vsno_obs_err ,  & ! observed snow volume std (m) 
-         vsno_gain    ,  & ! Kalman gain for vsno
-         vsno_mod_err ,  & ! modeled snow volume std (m) 
-         vsno_nw           ! optimal nudging weigth for snow volume
+         vsno_obs_err      ! observed snow volume std (m) 
 
       real (kind=dbl_kind), dimension (:,:,:,:), allocatable :: &
          trcr_obs, trcr_err     ! tracers
-
-      !-----------------------------------------------------------------
-      ! optimal nudging weigth for categories
-      !-----------------------------------------------------------------
-
-      real (kind=dbl_kind), dimension (:,:,:,:), allocatable :: &
-         aicen_nw , & ! nudging weight for SIC categories
-         vicen_nw , & ! for sea ice volume categories
-         vsnon_nw     ! for snow volume categories
-
-      real (kind=dbl_kind), dimension (:,:,:,:,:), allocatable :: &
-         trcrn_da     ! tracers
 
 !=======================================================================
 
@@ -91,7 +68,6 @@
  subroutine init_da
 
       use ice_blocks, only: block, get_block, nblocks_x, nblocks_y
-      use ice_communicate, only: my_task, master_task
       use ice_constants, only: c0
       use ice_domain, only: ew_boundary_type, ns_boundary_type, &
           nblocks, blocks_ice
@@ -113,6 +89,8 @@
    type (block) :: &
      this_block  ! block info for current block
 
+   call ice_timer_start(timer_da)
+
 !=======================================================================
    if (ew_boundary_type == 'open' .and. &
        ns_boundary_type == 'open' .and. .not.(restart_ext)) then
@@ -123,49 +101,26 @@
 
    if (da_sic) then
       allocate (aice_obs(nx_block,ny_block,max_blocks), &
-                aice_obs_err(nx_block,ny_block,max_blocks), &
-                aice_mod_err(nx_block,ny_block,max_blocks), &
-                aice_gain(nx_block,ny_block,max_blocks), &
-                aice_nw(nx_block,ny_block,max_blocks), &
-                aicen_nw(nx_block,ny_block,ncat,max_blocks))
+                aice_obs_err(nx_block,ny_block,max_blocks))
       aice_obs = c0
       aice_obs_err = c0
-      aice_mod_err = c0
-      aice_gain = c0
-      aice_nw = c0
-      aicen_nw = c0
    endif
 
    if (da_sit) then
       allocate (vice_obs(nx_block,ny_block,max_blocks), &
-                vice_obs_err(nx_block,ny_block,max_blocks), &
-                vice_mod_err(nx_block,ny_block,max_blocks), &
-                vice_gain(nx_block,ny_block,max_blocks), &
-                vice_nw(nx_block,ny_block,max_blocks), &
-                vicen_nw(nx_block,ny_block,ncat,max_blocks))
+                vice_obs_err(nx_block,ny_block,max_blocks))
       vice_obs = c0
       vice_obs_err = c0
-      vice_mod_err = c0
-      vice_gain = c0
-      vice_nw = c0
-      vicen_nw = c0
    endif
 
    if (da_sno) then
       allocate (vsno_obs(nx_block,ny_block,max_blocks), &
-                vsno_obs_err(nx_block,ny_block,max_blocks), &
-                vsno_mod_err(nx_block,ny_block,max_blocks), &
-                vsno_gain(nx_block,ny_block,max_blocks), &
-                vsno_nw(nx_block,ny_block,max_blocks), &
-                vsnon_nw(nx_block,ny_block,ncat,max_blocks))
+                vsno_obs_err(nx_block,ny_block,max_blocks))
       vsno_obs = c0
       vsno_obs_err = c0
-      vsno_mod_err = c0
-      vsno_gain = c0
-      vsno_nw = c0
-      vsnon_nw = c0
    endif
 
+   call ice_timer_stop(timer_da)
 
  end subroutine init_da
 
@@ -176,8 +131,6 @@
  subroutine ice_da_run
 
       use ice_blocks, only: block, get_block, nblocks_x, nblocks_y
-      use ice_calendar, only: dt
-      use ice_constants, only: secday
       use ice_domain, only: ew_boundary_type, ns_boundary_type, &
           nblocks, blocks_ice
 
@@ -192,7 +145,6 @@
      jglob(ny_block),    & ! global indices
      iblock, jblock,     & ! block indices
      ibc,                & ! ghost cell column or row
-     nda,                & ! number of model steps between two observations
      npad,               & ! padding column/row counter
      fid                   ! file id for netCDF routines
 
@@ -212,21 +164,30 @@
 !  Read observations
 !-----------------------------------------------------------------------
 
-   if (new_day) then
+   if ((istep == 1) .or. new_day) then
 
    ! sea ice concentration & uncertainties
       write(da_date,'(i4)') idate/10000
-
       data_file = trim(da_data_dir)//'osisaf_'//trim(da_date)//'.nc'
+      write(nu_diag,*) 'DA data file = ', data_file
+
       call ice_open_nc(data_file,fid)
 
       fieldname = 'obsAice'
       call ice_read_nc (fid, int(yday), fieldname, aice_obs, dbug, &
            field_loc_center, field_type_scalar)
-         
+        
       fieldname = 'obsAerr'
       call ice_read_nc (fid, int(yday), fieldname, aice_obs_err, dbug, &
            field_loc_center, field_type_scalar)
+
+      where ((aice_obs >= c0) .and. (aice_obs) <= 100)
+           aice_obs = aice_obs * p01
+           aice_obs_err = aice_obs_err * p01
+      elsewhere
+           aice_obs = c0
+           aice_obs_err = c0
+      endwhere
 
       call ice_close_nc(fid)
 
@@ -234,9 +195,6 @@
          
    if (da_method == 'coin') then
 
-      nda = secday / dt
-
-      ! perform data assimilation
       !$OMP PARALLEL DO PRIVATE(iblk,ilo,ihi,jlo,jhi,this_block, &
       !$OMP                     iglob,jglob,iblock,jblock)
       do iblk = 1, nblocks
@@ -250,43 +208,131 @@
          iblock = this_block%iblock
          jblock = this_block%jblock
 
-         if (da_sic == .true.) then
-
-            do j = jlo, jhi
-            do i = ilo, ihi
-
-               if (tmask(i,j,iblk)) then
-                  aice_mod_err(i,j,iblk) = aice(i,j,iblk) - aice_obs(i,j,iblk)
-                  aice_gain(i,j,iblk) = aice_mod_err(i,j,iblk)**2 /  &
-                       (aice_mod_err(i,j,iblk)**2 + puny + &
-                        aice_obs_err(i,j,iblk)**2)
-                  aice_nw(i,j,iblk) = c1 - (c1 - aice_gain(i,j,iblk)) &
-                       **(c1/real(nda,kind=dbl_kind))
-               else
-                  aice_nw(i,j,iblk) = c0
-               endif
-
-               if (aice(i,j,iblk) > c0) then
-                  do n=1, ncat
-                     aicen(i,j,n,iblk) = aicen(i,j,n,iblk) * (1 - aice_nw(i,j,iblk) &
-                        * (aice_obs(i,j,iblk)/aice(i,j,iblk) - 1))
-                  enddo
-               else
-                  if (tmask(i,j,iblk)) then
-                     aicen(i,j,1,iblk) = aicen(i,j,1,iblk) + aice_nw(i,j,iblk) * &
-                                        (aice_obs(i,j,iblk) - aicen(i,j,1,iblk))
-                  endif
-
-               endif
-            enddo
-            enddo
-         endif
-      enddo
+         call da_coin  (nx_block,            ny_block,      &
+                        ilo, ihi,            jlo, jhi,      &
+                        iglob,               jglob,         &
+                        iblock,              jblock,        &
+                        tmask(:,:,    iblk),                &
+                        aice(:,:,     iblk),                &
+                        aice_obs(:,:, iblk),                &
+                        aice_obs_err(:,:, iblk),            &
+                        aicen(:,:,  :,iblk),                &
+                        vicen(:,:,  :,iblk),                &
+                        vsnon(:,:,  :,iblk),                &
+                        trcrn(:,:,:,:,iblk), ntrcr)         
+      enddo ! iblk
+      !$OMP END PARALLEL DO
    endif
 
    call ice_timer_stop(timer_da)
 
 end subroutine ice_da_run
+
+!=======================================================================
+
+subroutine da_coin    (nx_block,            ny_block,      &
+                       ilo, ihi,            jlo, jhi,      &
+                       iglob,               jglob,         &
+                       iblock,              jblock,        &
+                       tmask,               aice,          &
+                       aice_obs,            aice_obs_err,  &
+                       aicen,     vicen,    vsnon,         &
+                       trcrn,     ntrcr)         
+
+      use ice_blocks, only: nblocks_x, nblocks_y
+
+      integer (kind=int_kind), intent(in) :: &
+         nx_block, ny_block, & ! block dimensions
+         ilo, ihi          , & ! physical domain indices
+         jlo, jhi          , & !
+         iglob(nx_block)   , & ! global indices
+         jglob(ny_block)   , & !
+         iblock            , & ! block indices
+         jblock            , & !
+         ntrcr                 ! number of tracers in use
+
+      logical (kind=log_kind), dimension (nx_block,ny_block), &
+         intent(inout) :: &
+         tmask                 ! true for ice/ocean cells
+
+      real (kind=dbl_kind), dimension (nx_block,ny_block), &
+         intent(in) :: &
+         aice,               & ! model aggregate sic
+         aice_obs,           & ! observed aggregate sic
+         aice_obs_err          ! observed aggregate sic_err
+         
+      real (kind=dbl_kind), dimension (nx_block,ny_block,ncat), &
+         intent(inout) :: &
+         aicen , & ! concentration of ice
+         vicen , & ! volume per unit area of ice          (m)
+         vsnon     ! volume per unit area of snow         (m)
+
+      real (kind=dbl_kind), dimension (nx_block,ny_block,ntrcr,ncat), &
+         intent(inout) :: &
+         trcrn     ! ice tracers
+                   ! 1: surface temperature of ice/snow (C)
+
+      ! local variables
+
+      integer (kind=int_kind) :: &
+         i, j        , & ! horizontal indices
+         ij          , & ! horizontal index, combines i and j loops
+         ibc         , & ! ghost cell column or row
+         npad        , & ! padding column/row counter
+         k           , & ! ice layer index
+         n           , & ! thickness category index
+         it          , & ! tracer index
+         icells          ! number of cells initialized with ice
+
+      integer (kind=int_kind), dimension(nx_block*ny_block) :: &
+         indxi, indxj    ! compressed indices for cells with restoring
+
+      real (kind=dbl_kind) :: &
+         mod_err,      & ! model error
+         gain,         & ! Kalman gain, optimal estimated
+         weight,       & ! nudging weight, increamental Kalman gain
+         weightn,      & ! nudging weigth distributed among categories
+         rda             ! dt/dT, where dT is observation time step
+
+      call ice_timer_start(timer_da)
+
+      !-----------------------------------------------------------------
+      ! assimilate sic on grid
+      !-----------------------------------------------------------------
+
+      rda = dt / real(secday,kind=dbl_kind)
+      if (my_task == master_task) write(nu_diag,*) 'rda = ', rda
+
+      if (da_sic == .true.) then
+
+         do j = 1, ny_block
+         do i = 1, nx_block
+            if (tmask(i,j)) then
+               mod_err = aice(i,j) - aice_obs(i,j)
+               gain   = mod_err**2/(mod_err**2+puny+aice_obs_err(i,j)**2)
+               weight = c1 - (c1 - gain)**rda
+            else
+               weight = c0
+            endif
+
+            if (aice(i,j) > p1) then
+               do n=1, ncat
+                  aicen(i,j,n) = aicen(i,j,n) * (c1 + weight &
+                        * (aice_obs(i,j)/aice(i,j) - c1))
+               enddo
+            else
+               if (tmask(i,j)) then
+                  aicen(i,j,1) = aicen(i,j,1) + weight * &
+                                (aice_obs(i,j) - aicen(i,j,1))
+               endif
+            endif
+         enddo
+         enddo
+      endif
+
+      call ice_timer_stop(timer_da)
+
+end subroutine da_coin
 
 !=======================================================================
 
