13a12ec683e80cfcb684561e10f8bcf151b405f8
Keguang Wang <keguang.wang@met.no>
2020-02-03 15:47:35 +0000

    add corr_bias

--- apps/common/modified_src/cice5.1.2/source/ice_init.F90
+++ apps/common/modified_src/cice5.1.2/source/ice_init.F90
@@ -92,7 +92,7 @@
                                  phi_i_mushy
       use ice_restoring, only: restore_ice
       use ice_da, only: da_ice, da_sic, da_sit, da_sno, da_method, &
-                        da_data_dir, Tobs
+                        da_data_dir, Tobs, corr_bias
 #ifdef CCSMCOUPLED
       use shr_file_mod, only: shr_file_setIO
 #endif
@@ -174,7 +174,7 @@
 
       namelist /da_nml/  &
         da_ice, da_sic, da_sit, da_sno, &
-        da_method, da_data_dir, Tobs
+        da_method, da_data_dir, Tobs, corr_bias
 
       !-----------------------------------------------------------------
       ! default values
@@ -337,6 +337,7 @@
       da_method   = 'coin'
       da_data_dir = '.'
       Tobs        = 86400.0_dbl_kind
+      corr_bias   = .true.
 
       !-----------------------------------------------------------------
       ! read from input file
@@ -770,6 +771,7 @@
       call broadcast_scalar(da_method,          master_task)
       call broadcast_scalar(da_data_dir,        master_task)
       call broadcast_scalar(Tobs,               master_task)
+      call broadcast_scalar(corr_bias,          master_task)
 
       if (dbug) & ! else only master_task writes to file
       call broadcast_scalar(nu_diag,            master_task)
