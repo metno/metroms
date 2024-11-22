00da0bb92f919dff9c8fafdbaaf52378876b4934
martinaidzo <martinai@met.no>
2024-11-22 15:20:01 +0000

    Revert "add r8_b ucx queue modules"
    This reverts commit 862e1fbbf22005cb126795034cd4c6d6f0cf85d3.

--- apps/myenv.bash
+++ apps/myenv.bash
@@ -8,23 +8,23 @@ fi
 # Always assign the first argument to METROMS_MYHOST
 METROMS_MYHOST=$1
 
-# If METROMS_MYHOST is met_ppi, check if second argument METROMS_COMPUTENODE is provided
+# If METROMS_MYHOST is met_ppi, check if second argument METROMS_LOGINNODE is provided
 if [ "$METROMS_MYHOST" == "met_ppi" ]; then
     # Check if a second argument was provided
     if [ $# -eq 2 ]; then
-        METROMS_COMPUTENODE=$2
+        METROMS_LOGINNODE=$2
     else
-        # If not, set the METROMS_COMPUTENODE variable to r8_b for met_ppi
-        METROMS_COMPUTENODE="r8_b"
-        echo "No login node option provided, loading $METROMS_COMPUTENODE login node"
+        # If not, set the METROMS_LOGINNODE variable to r8_b for met_ppi
+        METROMS_LOGINNODE="r8_b"
+        echo "No login node option provided, loading $METROMS_LOGINNODE login node"
     fi
 fi
 
 # Export the variables
 export METROMS_MYHOST
-export METROMS_COMPUTENODE
+export METROMS_LOGINNODE
 echo "Loading $METROMS_MYHOST paths"
-echo "Loading $METROMS_COMPUTENODE login node if METROMS_MYHOST is met_ppi"
+echo "Loading $METROMS_LOGINNODE login node if METROMS_MYHOST is met_ppi"
 
 if [ "$METROMS_MYHOST" == "met_ppi" ]; then
     if [ "$USER" == "havis" ]; then
